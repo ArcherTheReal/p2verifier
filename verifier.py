55f941b40fae315a53b7add8d3797ea47b50acae
Archer <62026087+ArcherTheReal@users.noreply.github.com>
2024-07-27 13:16:02 +0200

    feat: getPortal2Folder

--- verifier.py
+++ verifier.py
@@ -2,6 +2,7 @@ import os
 import json
 import winreg
 import datetime
+import vdf
 
 def log(message):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -18,12 +19,30 @@ def getSteamPath():
 
 def getPortal2Folder():
     steamPath = getSteamPath()
+
     if steamPath is None:
         return None
     
+    libraryFolders = os.path.join(steamPath, "steamapps", "libraryfolders.vdf")
+
+    if not os.path.exists(libraryFolders):
+        log("Failed to find libraryfolders.vdf")
+        return None
+    
+    libraryFolders = vdf.load(open(libraryFolders))["libraryfolders"]
+    for folder in libraryFolders.values():
+        apps = folder.get("apps", {})
+        if "620" in apps:  # '620' is the app ID for Portal 2
+            portal2Path = os.path.join(folder["path"], "steamapps", "common", "Portal 2")
+            if os.path.exists(portal2Path):
+                return portal2Path
+    return None
+
+
+
 
 # First setup
-if not os.path.exists('config.json'):
+if not os.path.exists("config.json"):
     # Default config
     configTemplate = {
         "path": os.path.dirname(os.path.abspath(__file__)),
@@ -39,5 +58,3 @@ if not os.path.exists('config.json'):
 
 if not os.path.exists('run'):
     os.makedirs('run')
-
-print(getPortal2Folder())
