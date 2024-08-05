e565e5e97c991662fbca268d4d7c52d69b3f36b0
Archer <tippierarcher@gmail.com>
2024-08-06 00:26:28 +0200

    feat: add auto updating verifier and mdp

--- setup.py
+++ setup.py
@@ -1,19 +1,82 @@
 import os
 import subprocess
-from verifier.colors import Colors
-from verifier import config
+import importlib
+import zipfile
+import shutil
+import requests
 
-git_repo = "https://github.com/ArcherTheReal/p2verifier.git"
+git_repo = "ArcherTheReal/p2verifier"
+mdp_repo = "p2sr/mdp"
 
-config.reset_config()
-
-ans = input(Colors.colorize("WARNING: This will overwrite all files of the project. Are you sure you want to continue? (y/n) ", Colors.RED))
+ans = input("\033[31mWARNING: This will overwrite all files of the project. Are you sure you want to continue? (y/n) \033[0m")
 if ans.lower() != "y":
     exit()
 
 
+def get_release(repo, version = None):
+    url = f""
+    if version:
+        url += f"tags/{version}"
+    else:
+        url += "latest"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def update_verifier(repo, target_folder):
+
+    release = requests.get(f"https://api.github.com/repos/{repo}/releases/latest").json()
+    online_version = release['tag_name']
+
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    
+    zip_url = release['zipball_url']
+    response = requests.get(zip_url)
+    zip_path = os.path.join(target_folder, 'source.zip')
+    with open(zip_path, 'wb') as file:
+        file.write(response.content)
+
+
+    # Unzip the file
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        os.makedirs(os.path.join(target_folder, "upstream"), exist_ok=True)
+        zip_ref.extractall(os.path.join(target_folder, "upstream"))
+        
+        extracted_folder = os.listdir(os.path.join(target_folder, "upstream"))[0]
+        extracted_folder = os.path.join(target_folder, "upstream", extracted_folder)
+        if extracted_folder:
+            # Move contents from the extracted folder to the target_folder
+            for filename in os.listdir(extracted_folder):
+                old_path = os.path.join(extracted_folder, filename)
+                new_path = os.path.join(target_folder, filename)
+                if os.path.isdir(old_path):
+                    if os.path.exists(new_path):
+                        shutil.rmtree(new_path)
+                    shutil.move(old_path, target_folder)
+                else:
+                    if os.path.exists(new_path):
+                        os.remove(new_path)
+                    shutil.move(old_path, target_folder)
+            # Remove the now-empty subfolder
+            os.rmdir(extracted_folder)
+    # Remove the zip file
+    os.remove(zip_path)
+
+    with open(os.path.join(target_folder, "version.txt"), 'w') as file:
+        file.write(online_version)
+
+
+update_verifier(git_repo, os.path.dirname(os.path.abspath(__file__)))
+
 subprocess.run(["pip", "install", "-r", "requirements.txt"])
 
+config = importlib.import_module("verifier.config")
+updater = importlib.import_module("verifier.updater")
+
 os.makedirs("run", exist_ok=True)
+
+updater.update_mdp(mdp_repo, "mdp")
 
 config.validate_files()
---
+++ verifier/updater.py
@@ -0,0 +1,106 @@
+import os
+import requests
+import zipfile
+import shutil
+
+from verifier.logger import log, error
+
+def get_release(repo, version = None):
+    url = f"https://api.github.com/repos/{repo}/releases/"
+    if version:
+        url += f"tags/{version}"
+    else:
+        url += "latest"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+def download_file(url, dest):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    with open(dest, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+
+def read_local_version(version_file):
+    if os.path.exists(version_file):
+        with open(version_file, 'r') as file:
+            return file.read().strip()
+    return None
+
+def write_local_version(version_file, version):
+    with open(version_file, 'w') as file:
+        file.write(version)
+
+def update_mdp(repo, target_folder, version = None):
+    release = get_release(repo, version)
+    online_version = release['tag_name']
+
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    
+    version_file = os.path.join(target_folder, 'version.txt')
+    local_version = read_local_version(version_file)
+
+    if local_version == online_version:
+        log("mdp is already up-to-date.")
+        return
+
+    log(f"Updating mdp.exe from version {local_version} to {online_version}")
+
+    exe_asset = next(asset for asset in release['assets'] if asset['name'] == 'mdp.exe')
+    exe_url = exe_asset['browser_download_url']
+
+    exe_path = os.path.join(target_folder, 'mdp.exe')
+    download_file(exe_url, exe_path)
+
+    # Write the latest version to the version file
+    write_local_version(version_file, online_version)
+
+    log(f"mdp has been updated to version: {online_version}")
+
+def update_verifier(repo, target_folder, version = None):
+    release = get_release(repo, version)
+    online_version = release['tag_name']
+
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    
+    version_file = os.path.join(target_folder, 'version.txt')
+    local_version = read_local_version(version_file)
+
+    if local_version == online_version:
+        log("verifier is already up-to-date.")
+        return
+
+    download_file(release['zipball_url'], os.path.join(target_folder, 'source.zip'))
+
+
+    # Unzip the file
+    with zipfile.ZipFile(os.path.join(target_folder, 'source.zip'), 'r') as zip_ref:
+        os.makedirs(os.path.join(target_folder, "upstream"), exist_ok=True)
+        zip_ref.extractall(os.path.join(target_folder, "upstream"))
+        
+        extracted_folder = os.listdir(os.path.join(target_folder, "upstream"))[0]
+        extracted_folder = os.path.join(target_folder, "upstream", extracted_folder)
+        if extracted_folder:
+            # Move contents from the extracted folder to the target_folder
+            for filename in os.listdir(extracted_folder):
+                old_path = os.path.join(extracted_folder, filename)
+                new_path = os.path.join(target_folder, filename)
+                if os.path.isdir(old_path):
+                    if os.path.exists(new_path):
+                        shutil.rmtree(new_path)
+                    shutil.move(old_path, target_folder)
+                else:
+                    if os.path.exists(new_path):
+                        os.remove(new_path)
+                    shutil.move(old_path, target_folder)
+            # Remove the now-empty subfolder
+            os.rmdir(extracted_folder)
+    # Remove the zip file
+    os.remove(zip_path)
+
+    
+    write_local_version(version_file, online_version)
+    log(f"verifier has been updated to version: {online_version}")
