62cf4824eb76328779b9b976e76274d00a79e9f2
Archer <62026087+ArcherTheReal@users.noreply.github.com>
2024-08-05 20:07:15 +0200

    feat: error logging

--- verifier/logger.py
+++ verifier/logger.py
@@ -2,8 +2,9 @@ import datetime
 from verifier.colors import Colors
 
 def log(message):
-    """
-    Logs a message with a timestamp, using blue for the timestamp and green for the message.
-    """
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{Colors.colorize(timestamp, Colors.BLUE)}] {Colors.colorize(message, Colors.GREEN)}")
+    print(f"[{Colors.colorize(timestamp, Colors.BLUE)}] {Colors.colorize(message, Colors.ORANGE)}")
+
+def error(message):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{Colors.colorize(timestamp, Colors.BLUE)}] {Colors.colorize(message, Colors.RED)}")
