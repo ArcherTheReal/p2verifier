6c5b41cd7ae1ecc7c73388356fcf43f802300a13
Archer <62026087+ArcherTheReal@users.noreply.github.com>
2024-08-05 20:09:20 +0200

    feat: main file

---
+++ main.py
@@ -0,0 +1,47 @@
+import asyncio
+import json
+import os
+import subprocess
+
+from verifier.config import validate_files, load_config, setup_paths
+from verifier.logger import log, error
+from verifier.files import clear_folders, copy_demos
+from verifier.mdp import init_mdp, sort_demos
+from verifier.telnet import fetch_server_nums
+from verifier.utils import fill_output, cli
+from verifier.verifier import Verifier
+
+
+
+verifier = Verifier()
+
+async def main():
+    validate_files()
+    verifier.config = load_config()
+    if not verifier.config:
+        error("Failed to load config")
+        input("Press a key to exit")
+        exit(1)
+
+    setup_paths(verifier)
+
+    clear_folders(verifier)
+    copy_demos(verifier)
+
+    init_mdp(verifier)
+    sort_demos(verifier)
+
+    await fetch_server_nums(verifier)
+    if verifier.portal2Process.poll() is None:
+        verifier.portal2Process.terminate()
+
+    fill_output(verifier)
+    with open("output.json", "w") as f:
+        f.write(json.dumps(verifier.output, indent=4))
+
+    if verifier.config["options"]["commandline"]:
+        log("Running Portal 2")
+        await cli(verifier)
+
+if __name__ == "__main__":
+    asyncio.run(main())
